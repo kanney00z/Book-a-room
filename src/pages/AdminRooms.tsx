24c76c1611c2f01ffe42027585c2200937d891b6
Dev <dev@example.com>
2026-04-23 09:20:14 +0700

    Fix React namespace error

--- src/pages/AdminRooms.tsx
+++ src/pages/AdminRooms.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useData } from '../lib/DataContext';
 import { supabase } from '../lib/supabase';
 import { Room, RoomStatus } from '../types';
